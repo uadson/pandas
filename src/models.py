1e411c5bdff54b7a90b68e5d4c7c8063417ae3a3
uadson <uadsoncastro@gmail.com>
2022-02-11 07:57:03 -0300

    intro data analysis

--- src/models.py
+++ src/models.py
@@ -10,7 +10,10 @@ class Analysis:
 		self.dataset = dict()
 
 	def get_data(self):
+		# objects recebe um dicionario com chave int e valor = arquivo com extensão csv
 		objects = {num + 1: elem for num, elem in enumerate(datalist)}
+
+		# loop for para visualização das chaves e valores no terminal
 		for key, value in objects.items():
 			print(key, '-', value)
 		option = int(input('\nSelect dataset: '))
@@ -20,4 +23,4 @@ class Analysis:
 
 if __name__ == '__main__':
 	dataset = Analysis()
-	print(dataset.get_data())
+	dataset.get_data()
